2d8857e31e382b48312f97f78291697f53fa7a80
jeanarm <nizigiarmel2015@gmail.com>
2019-11-25 09:24:12 +0000

    last commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191123200542) do
+ActiveRecord::Schema.define(version: 20191125091941) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "property"
