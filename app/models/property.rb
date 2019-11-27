d87f0909c9beae7d0a357233218cc79ff7dc5abd
mediateri <ntawukuriryayomeddy@gmail.com>
2019-11-27 13:20:13 +0000

    done

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,11 @@
 class Property < ApplicationRecord
     validates :property, :rent, :adress,
         :building_age, presence: true
-    validates :building_age, :how_many_minutes_walks, :how_many_minutes_walks1, numericality: true
+        def check
+            if how_many_minutes_walks.present? && how_many_minutes_walks1.present?
+                if how_many_minutes_walks <= how_many_minutes_walks1
+                    errors.add(:how_many_minutes_walks, 'not perfect')
+                end
+            end
+        end
 end
